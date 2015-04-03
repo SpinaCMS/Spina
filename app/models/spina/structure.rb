f39baf8b6c0567300f0b519ad74fb31a030b8688
Bram Jetten <mail@bramjetten.nl>
2015-04-03 10:51:33 +0200

    Structure page partables

--- app/models/spina/structure.rb
+++ app/models/spina/structure.rb
@@ -1,6 +1,6 @@
 module Spina
   class Structure < ActiveRecord::Base
-    has_many :page_parts, as: :page_partable
+    has_one :page_part, as: :page_partable
     has_many :structure_items
 
     accepts_nested_attributes_for :structure_items, allow_destroy: true
