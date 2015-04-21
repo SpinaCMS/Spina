10c251dd8ca8acec5b4d9d9a742b811dc4f70a0b
Bram Jetten <mail@bramjetten.nl>
2015-04-21 12:34:59 +0200

    Refactoring pages_helper.rb

--- app/helpers/spina/admin/pages_helper.rb
+++ app/helpers/spina/admin/pages_helper.rb
@@ -5,26 +5,26 @@ module Spina
         new_object = f.object.send(association).klass.new
         id = new_object.object_id
         fields = f.fields_for(association, new_object, child_index: id) do |builder|
+          build_structure_parts(f.object.page_part.name, new_object) if structure_item?(new_object)
           render(association.to_s.singularize + "_fields", f: builder)
         end
-        link_to '#', class: "add_fields button", data: {id: id, fields: fields.gsub("\n", "")} do
+        link_to '#', class: "#{add_fields_class(new_object)} button button-link", data: {id: id, fields: fields.gsub("\n", "")} do
           block.yield
         end
       end
 
-      def link_to_add_structure(f, association, &block)
-        new_object = f.object.send(association).klass.new
-        id = new_object.object_id
-        fields = f.fields_for(association, new_object, child_index: id) do |builder|
-          current_theme.config.structures[f.object.page_part.name].each do |structure_part|
-            structure_part = new_object.structure_parts.build(structure_part)
-            structure_part.structure_partable = structure_part.structure_partable_type.constantize.new
-          end
+      def add_fields_class(object)
+        structure_item?(object) ? 'add_structure' : 'add_fields'
+      end
 
-          render(association.to_s.singularize + "_fields", f: builder)
-        end
-        link_to '#', class: "add_structure button button-link", data: {id: id, fields: fields.gsub("\n", "")} do
-          block.yield
+      def structure_item?(object)
+        object.class.name.demodulize == "StructureItem"
+      end
+
+      def build_structure_parts(name, item)
+        current_theme.config.structures[name].each do |part|
+          part = item.structure_parts.build(part)
+          part.structure_partable = part.structure_partable_type.constantize.new
         end
       end
     end
