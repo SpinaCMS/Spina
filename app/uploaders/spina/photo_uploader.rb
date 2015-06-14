6d86919260a0ddd8fd1bb57e158b76530983f059
Dean Perry <dean@deanperry.net>
2015-06-14 17:21:30 +0100

    s3 support by setting some extra configs

--- app/uploaders/spina/photo_uploader.rb
+++ app/uploaders/spina/photo_uploader.rb
@@ -2,20 +2,8 @@
 module Spina
   class PhotoUploader < CarrierWave::Uploader::Base
 
-    # Include RMagick or MiniMagick support:
-    # include CarrierWave::RMagick
     include CarrierWave::MiniMagick
 
-    # Include the Sprockets helpers for Rails 3.1+ asset pipeline compatibility:
-    # include Sprockets::Helpers::RailsHelper
-    # include Sprockets::Helpers::IsolatedHelper
-
-    # Choose what kind of storage to use for this uploader:
-    storage :file
-    # storage :fog
-
-    # Override the directory where uploaded files will be stored.
-    # This is a sensible default for uploaders that are meant to be mounted:
     def store_dir
       "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
     end
@@ -24,7 +12,6 @@ module Spina
       process resize_to_fit: [800, 800], if: :too_large?
     end
 
-    # Create different versions of your uploaded files:
     version :thumb do
       process resize_to_fill: [240, 135]
     end
@@ -33,10 +20,9 @@ module Spina
       new_file.size > 120 * 1000
     end
 
-    # Add a white list of extensions which are allowed to be uploaded.
-    # For images you might use something like this:
     def extension_white_list
       %w(jpg jpeg gif png)
     end
+
   end
 end
