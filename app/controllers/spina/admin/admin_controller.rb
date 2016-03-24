f4b377812c011e11143702448f6f2265e651ca2f
Louis Larpin <louis.larpin@gmail.com>
2016-03-24 16:02:45 +0100

    Avoid class name collision with main app

--- app/controllers/spina/admin/admin_controller.rb
+++ app/controllers/spina/admin/admin_controller.rb
@@ -1,7 +1,7 @@
 module Spina
   module Admin
     class AdminController < ApplicationController
-      
+
       before_filter :authorize_user
       before_filter :new_messages
 
@@ -21,9 +21,9 @@ module Spina
         @current_account ||= Account.first
       end
       helper_method :current_account
-      
+
       def current_user
-        @current_user ||= User.where(id: session[:user_id]).first if session[:user_id]
+        @current_user ||= Spina::User.where(id: session[:user_id]).first if session[:user_id]
       end
       helper_method :current_user
 
