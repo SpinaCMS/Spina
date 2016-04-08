da6022aee08a834dab152f802d0302830f413e75
Harm de Wit <harmdewit@gmail.com>
2016-04-08 14:52:37 +0200

    Small fixes after merge

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160324153108) do
+ActiveRecord::Schema.define(version: 20160408124736) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
