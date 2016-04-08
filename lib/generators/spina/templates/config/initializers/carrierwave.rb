766e155c896248dd4fa64dfd5c1b019847bba7d0
Bram Jetten <mail@bramjetten.nl>
2016-04-08 14:50:45 +0200

    Cnf to config

--- lib/generators/spina/templates/config/initializers/carrierwave.rb
+++ lib/generators/spina/templates/config/initializers/carrierwave.rb
@@ -2,16 +2,16 @@ CarrierWave.configure do |config|
   config.storage = :file
 
   # Use S3 if you want
-  # cfg.storage = :fog
-  # cfg.fog_credentials = {
+  # config.storage = :fog
+  # config.fog_credentials = {
   #   provider:               'AWS',
   #   region:                 '',
   #   aws_access_key_id:      '',
   #   aws_secret_access_key:  ''
   # }
-  # cfg.fog_directory  = ''
-  # cfg.fog_public     = true
-  # cfg.fog_attributes = { 'Cache-Control' => 'max-age=315576000' }
+  # config.fog_directory  = ''
+  # config.fog_public     = true
+  # config.fog_attributes = { 'Cache-Control' => 'max-age=315576000' }
 
   config.enable_processing = !Rails.env.test?
 end
