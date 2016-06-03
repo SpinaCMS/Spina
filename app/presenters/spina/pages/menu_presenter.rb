7af15d7c03f28bd60048889d1359c412466d316b
Cedric Deschenes <cedricdeschenes@gmail.com>
2016-06-03 14:43:52 -0400

    Added verification if an item has childs, extra variable for css

--- app/presenters/spina/pages/menu_presenter.rb
+++ app/presenters/spina/pages/menu_presenter.rb
@@ -10,7 +10,7 @@ module Spina
       include ActionView::Helpers::UrlHelper
       include ActiveSupport::Configurable
 
-      config_accessor :list_tag, :list_class, :li, :list_wrapper, :list_item_tag, :list_item_css, :selected_css, :current_css, :first_css, :last_css
+      config_accessor :list_tag, :list_class, :li, :list_wrapper, :list_item_tag, :list_item_css, :selected_css, :current_css, :first_css, :last_css, :list_tag_with_child_css, :list_item_with_child_css
 
       self.list_tag = :ul
       self.list_class = 'nav'
@@ -21,6 +21,8 @@ module Spina
       self.current_css = 'current'
       self.first_css = 'first'
       self.last_css = 'last'
+      self.list_tag_with_child_css = 'dropdown-menu'
+      self.list_item_with_child_css = 'dropdown'
 
       attr_accessor :context, :collection, :current_menu_item
       delegate :output_buffer, :output_buffer=, to: :context
@@ -51,7 +53,7 @@ module Spina
       end
 
       def render_menu_items(menu_items)
-        content_tag(list_tag, class: list_class) do
+        content_tag(list_tag, class: menu_items_css(menu_items)) do
           menu_items.each_with_index.inject(ActiveSupport::SafeBuffer.new) do |buffer, (item, index)|
             buffer << render_menu_item(item, index, menu_items.length)
           end
@@ -59,7 +61,7 @@ module Spina
       end
 
       def render_menu_item(menu_item, index, menu_items_length)
-        content_tag(list_item_tag, class: menu_item_css(menu_item[0], index, menu_items_length)) do
+        content_tag(list_item_tag, class: menu_item_css(menu_item[0], index, menu_items_length, !menu_item[1].empty?)) do
           buffer = ActiveSupport::SafeBuffer.new
           buffer << link_to(menu_item[0].menu_title, menu_item[0].full_materialized_path)
           buffer << render_list_wrapper(menu_item[1])
@@ -67,13 +69,22 @@ module Spina
         end
       end
 
-      def menu_item_css(menu_item, index, menu_items_length)
+      def menu_items_css(menu_items)
+        css = []
+        css << list_class
+        css << list_tag_with_child_css if !menu_items.first[0].ancestry.nil?
+
+        css.reject(&:blank?).presence
+      end
+
+      def menu_item_css(menu_item, index, menu_items_length, has_child)
         css = []
         css << list_item_css
         css << selected_css if (@selected_menu_items.present? && @selected_menu_items.include?(menu_item) )
         css << current_css if @current_menu_item == menu_item
         css << first_css if index == 0
         css << last_css if index + 1 == menu_items_length
+        css << list_item_with_child_css if has_child
 
         css.reject(&:blank?).presence
       end
