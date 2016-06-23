00f75d01343bd5a08114ee96b56ff138d9feb19f
Bram Jetten <mail@bramjetten.nl>
2016-06-23 17:15:54 +0200

    Lets use cancancan instead of cancan

--- config/routes.rb
+++ config/routes.rb
@@ -71,7 +71,7 @@ Spina::Engine.routes.draw do
   get '/:locale/*id' => 'pages#show', constraints: {locale: /#{Spina.config.locales.join('|')}/ }
   get '/:locale/' => 'pages#homepage', constraints: {locale: /#{Spina.config.locales.join('|')}/ }
   get '/*id' => 'pages#show', as: "page", controller: 'pages', constraints: lambda { |request|
-    !(Rails.env.development? && request.env['PATH_INFO'].starts_with?('/rails/'))
+    !(Rails.env.development? && request.env['PATH_INFO'].starts_with?('/rails/') || request.env['PATH_INFO'].starts_with?('/attachments/'))
   }
 
 end
