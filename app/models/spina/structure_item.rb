cd54c6819250c9cfdaf88b065cba0daa04986e32
Thomas Bush <ThomasJosephBush@gmail.com>
2016-07-19 12:00:06 -0400

    ensures position is created on a new structure item

--- app/models/spina/structure_item.rb
+++ app/models/spina/structure_item.rb
@@ -1,14 +1,20 @@
 module Spina
   class StructureItem < ApplicationRecord
+    before_validation :ensure_position
     belongs_to :structure
     has_many :structure_parts, dependent: :destroy
 
     scope :sorted_by_structure, -> { order('position') }
 
+    validates_presence_of :position
     accepts_nested_attributes_for :structure_parts, allow_destroy: true
 
     def content(structure_part)
       structure_parts.find_by(name: structure_part).try(:content)
+    end
+
+    def ensure_position
+      self.position ||= Time.now.to_i
     end
   end
 end
