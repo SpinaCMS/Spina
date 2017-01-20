6131bc6231adf649a7997a86455f1cc0bbc5b53b
Dan Mitchell <dan@digitalreflow.co.uk>
2017-01-20 10:36:11 +0000

    The way Spina adds nested attributes with structure items is very specific and the current code clashes with 3rd party gems such as Cacoon which is nice to add to plugins for more standard nested form associations.

--- app/helpers/spina/admin/pages_helper.rb
+++ app/helpers/spina/admin/pages_helper.rb
@@ -1,20 +1,20 @@
 module Spina
   module Admin
     module PagesHelper
-      def link_to_add_fields(f, association, &block)
+      def link_to_add_structure_item_fields(f, association, &block)
         new_object = f.object.send(association).klass.new
         id = new_object.object_id
         fields = f.fields_for(association, new_object, child_index: id) do |builder|
           build_structure_parts(f.object.page_part.name, new_object) if structure_item?(new_object)
           render("spina/admin/#{ partable_partial_namespace(new_object) }/fields", f: builder)
         end
-        link_to '#', class: "#{add_fields_class(new_object)} button button-link", data: {id: id, fields: fields.gsub("\n", "")} do
+        link_to '#', class: "#{add_structure_item_fields_class(new_object)} button button-link", data: {id: id, fields: fields.gsub("\n", "")} do
           block.yield
         end
       end
 
-      def add_fields_class(object)
-        structure_item?(object) ? 'add_structure' : 'add_fields'
+      def add_structure_item_fields_class(object)
+        structure_item?(object) ? 'add_structure' : 'add_structure_item_fields'
       end
 
       def structure_item?(object)
