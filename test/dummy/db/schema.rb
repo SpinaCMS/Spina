df4c762dd09d1c31f0c422e4efe10c7a6b4e2962
Bram Jetten <mail@bramjetten.nl>
2017-06-03 13:03:02 +0200

    Add to readme

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 5) do
+ActiveRecord::Schema.define(version: 20170507153143) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -90,6 +90,12 @@ ActiveRecord::Schema.define(version: 5) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.index ["name"], name: "index_spina_navigations_on_name", unique: true, using: :btree
+  end
+
+  create_table "spina_options", force: :cascade do |t|
+    t.string   "value"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "spina_page_parts", force: :cascade do |t|
