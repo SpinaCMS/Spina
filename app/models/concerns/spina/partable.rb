13454d6e36331164117cff683a9494fd53d55cf0
Tom Simnett <tom@initforthe.com>
2017-07-17 12:48:22 +0100

    Move to concerns

--- app/models/concerns/spina/partable.rb
+++ app/models/concerns/spina/partable.rb
@@ -1,11 +1,15 @@
 module Spina
   module Partable
-    def part(part)
-      options = part[:options]
-      part = parts.where(name: part[:name]).first || parts.build(part)
-      part.options = options unless part.options
-      part.partable = part.partable_type.constantize.new unless part.partable.present?
-      part
+    extend ActiveSupport::Concern
+
+    included do
+      def part(part)
+        options = part[:options] || {}
+        part = parts.where(name: part[:name]).first || parts.build(part)
+        part.options = options unless part.options
+        part.partable = part.partable_type.constantize.new unless part.partable.present?
+        part
+      end
     end
   end
 end
