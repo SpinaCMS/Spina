91ccc37ba6b791831b31a9500cdd44dfdbfd1fb2
Tom Simnett <tom+github@initforthe.com>
2017-07-21 10:56:51 +0100

    Feature/fix admin forms (#283)
    * Move to concerns
    
    * Make a shortcut method for getting the value from an option part
    
    * include partable methods in s. Clean up existing methods
    
    * Ensure tests continue to pass
    
    * Fix Admin form parts, particularly for structures

--- app/models/concerns/spina/partable.rb
+++ app/models/concerns/spina/partable.rb
@@ -1,11 +1,17 @@
 module Spina
   module Partable
-    def part(part)
-      options = part[:options]
-      part = parts.where(name: part[:name]).first || parts.build(part)
-      part.options = options unless part.options
-      part.partable = part.partable_type.constantize.new unless part.partable.present?
-      part
+    extend ActiveSupport::Concern
+
+    included do
+      def part(part)
+        options = part[:options] || {}
+        part = parts.where(name: part[:name]).first || parts.build(part)
+        part.options = options unless part.options
+        if part.partable_type.present?
+          part.partable = part.partable_type.constantize.new unless part.partable.present?
+        end
+        part
+      end
     end
   end
 end
