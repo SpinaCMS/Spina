739e0f9d992d36da75f4fe8e05875046144b9b20
Bram Jetten <mail@bramjetten.nl>
2017-07-24 12:56:45 +0200

    Typo in config.to_prepare

--- lib/spina/engine.rb
+++ lib/spina/engine.rb
@@ -21,11 +21,13 @@ module Spina
     config.assets.paths << config.root.join('vendor', 'assets')
 
     config.to_prepare do
+      # Require decorators from main application
       [Rails.root].flatten.map { |p| Dir[p.join('app', 'decorators', '**', '*_decorator.rb')]}.flatten.uniq.each do |decorator|
         Rails.configuration.cache_classes ? require(decorator) : load(decorator)
-
-      Spina::ApplicationController.helper Rails.application.helpers
       end
+
+      # Load helpers from main application
+      Spina::ApplicationController.helper Rails.application.helpers
     end
 
   end
