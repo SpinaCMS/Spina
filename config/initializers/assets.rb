5058f8b7876701970bf30c6c1ec2e95aa4ee5a77
Bram Jetten <mail@bramjetten.nl>
2017-11-25 22:48:37 +0100

    Cleanup assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,1 +1,1 @@
-Rails.application.config.assets.precompile += %w(spina/spina.png spina/admin/spina.png spina/spina_small.png spina/media_folder_placeholder.png)
+Rails.application.config.assets.precompile += %w(spina/spina.png spina/spina-large.png spina/media_folder_placeholder.png)
