86c6454c2836f72bc78244dda14d1675b0660ea6
Bram Jetten <mail@bramjetten.nl>
2018-03-27 22:11:11 +0200

    Current Page

--- app/helpers/spina/pages_helper.rb
+++ app/helpers/spina/pages_helper.rb
@@ -2,15 +2,15 @@ module Spina
   module PagesHelper
 
     def page_content(part_name)
-      current_page.content(part_name)
+      page.content(part_name)
     end
 
     def page_has_content?(part_name)
-      current_page.has_content?(part_name)
+      page.has_content?(part_name)
     end
 
     def current_page
-      @page
+      Current.page
     end
 
   end
