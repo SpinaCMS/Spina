4a236402a235203e7db29053a67c470458d452bd
onordgren <otto@railers.se>
2018-05-05 14:17:55 +0200

    fixed wrong image/file reference (#365)

--- app/helpers/spina/images_helper.rb
+++ app/helpers/spina/images_helper.rb
@@ -5,8 +5,8 @@ module Spina
     # because the resolve directive in ActiveStorage's routes
     # doesn't work outside the main app in 5.2.0.rc2
     def variant(image, options)
-      if image.file.attached?
-        variant = image.file.variant(options)
+      if image.attached?
+        variant = image.variant(options)
         main_app.rails_blob_representation_path(variant.blob.signed_id, variant.variation.key, variant.blob.filename)
       else
         "https://placehold.it/100x100.png"
