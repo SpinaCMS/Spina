ee295bcbc87ec13c34477703770d7c8187f2fafd
Bram Jetten <mail@bramjetten.nl>
2018-05-23 13:00:26 +0200

    Frontend @page

--- db/migrate/11_add_image_references_to_photo_models.rb
+++
@@ -1,6 +0,0 @@
-class AddImageReferencesToPhotoModels < ActiveRecord::Migration[5.2]
-  def change
-    add_column :spina_photos, :image_id, :integer,
-               foreign_key: { to_table: :spina_images }
-  end
-end
