7214431108547d76bf89dfbe8cbd1391a96d76f8
Bram Jetten <mail@bramjetten.nl>
2018-06-05 22:11:05 +0200

    Remove image - Fix #376

--- app/controllers/spina/admin/media_picker_controller.rb
+++ app/controllers/spina/admin/media_picker_controller.rb
@@ -17,7 +17,7 @@ module Spina
 
       def select
         if params[:multiple]
-          @images = Image.find(params[:image_ids])
+          @images = Image.where(id: params[:image_ids])
         else 
           @image = Image.find(params[:image_id])
         end
