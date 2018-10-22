a2dfee8527ded426b9d7a9266dac21b42c6b9465
David Podroužek <podruh01@gmail.com>
2018-10-22 18:30:28 +0200

    Add svg content type check

--- app/models/spina/image.rb
+++ app/models/spina/image.rb
@@ -15,7 +15,10 @@ module Spina
     end
 
     def variant(options)
-      file.attached? ? file.variant(options) : ""
+      return "" unless file.attached?
+      return file if file.content_type.include?('svg')
+
+      file.variant(options)
     end
 
     def content
