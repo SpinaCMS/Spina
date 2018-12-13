13a8fbd8a7e724b6b4de65a69d84dfdb679859e8
Tomislav Simnett <tom@initforthe.com>
2018-12-13 15:55:21 +0000

    Use sprockets for loading fontawesome

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_30_095415) do
+ActiveRecord::Schema.define(version: 11) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,7 +23,6 @@ ActiveRecord::Schema.define(version: 2018_05_30_095415) do
     t.string "phone"
     t.string "email"
     t.text "preferences"
-    t.string "logo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "robots_allowed", default: false
