8fb6004b4e72f0d7c1fb8aaefccf38aea2b43438
dankmitchell <dan@danmitchell.co.uk>
2019-03-25 22:20:24 +0000

    Bump factory_bot

--- test/factories/pages.rb
+++ test/factories/pages.rb
@@ -1,25 +1,25 @@
 FactoryBot.define do
   factory :page, class: Spina::Page do
-    draft false
-    active true
+    draft { false }
+    active { true }
 
     factory :homepage do
-      name 'homepage'
-      title 'Homepage'
-      deletable false
+      name { 'homepage' }
+      title { 'Homepage' }
+      deletable { false }
     end
 
     factory :about_page do
-      name 'about'
-      title 'About'
-      deletable false
+      name { 'about' }
+      title { 'About' }
+      deletable { false }
     end
 
     factory :demo_page do
-      name 'demo'
-      title 'Demo'
-      deletable true
-      active false
+      name { 'demo' }
+      title { 'Demo' }
+      deletable { true }
+      active { false }
     end
 
   end
