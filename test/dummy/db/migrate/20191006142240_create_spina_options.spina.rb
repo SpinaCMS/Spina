4657eba121b5d644df059d42bf2026c5451485b0
Bram Jetten <mail@bramjetten.nl>
2019-10-06 17:31:21 +0200

    Delete copied migrations to prevent duplicate migration error when setting up the test db

--- test/dummy/db/migrate/20191006142240_create_spina_options.spina.rb
+++
@@ -1,9 +0,0 @@
-# This migration comes from spina (originally 6)
-class CreateSpinaOptions < ActiveRecord::Migration[5.0]
-  def change
-    create_table :spina_options do |t|
-      t.string :value
-      t.timestamps
-    end
-  end
-end
