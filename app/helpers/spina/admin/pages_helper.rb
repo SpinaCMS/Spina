974eba6f9fd9aec6a7494cf03bd9fe8a83bc074b
Bram Jetten <mail@bramjetten.nl>
2020-04-11 17:11:23 +0200

    Remove unnecessary helper methods

--- app/helpers/spina/admin/pages_helper.rb
+++ app/helpers/spina/admin/pages_helper.rb
@@ -1,16 +1,6 @@
 module Spina
   module Admin
     module PagesHelper
-      def link_to_add_structure_item_fields(f, &block)
-        item = StructureItem.new
-        fields = f.fields_for(:structure_items, item, child_index: item.object_id) do |builder|
-          build_structure_parts(f.object.page_part.name, item)
-          render("spina/admin/structure_items/fields", f: builder)
-        end
-        link_to '#', class: "add_structure_item_fields button button-link", data: {id: item.object_id, fields: fields.gsub("\n", "")} do
-          icon('plus')
-        end
-      end
 
       def link_to_add_repeater_fields(f, &block)
         repeater_content = Spina::Parts::RepeaterContent.new(name: "repeater", title: "Title")
@@ -28,25 +18,6 @@ module Spina
             partable.part(part_attributes)
           end
         end
-      end
-
-      def build_structure_parts(name, item)
-        structure = current_theme.structures.find { |structure| structure[:name] == name }
-        return item.parts unless structure.present?
-        structure[:structure_parts].map do |attributes|
-          part = item.parts.where(name: attributes[:name]).first_or_initialize(attributes)
-          part.partable = part.partable_type.constantize.new if part.partable.blank?
-          part.options = attributes[:options]
-          part
-        end
-      end
-
-      def partable_partial_namespace(partable)
-        partable_type_partial_namespace(partable.model_name.to_s)
-      end
-
-      def partable_type_partial_namespace(partable_type)
-        partable_type.tableize.sub(/\Aspina\//, '')
       end
 
       def parts_partial_namespace(part_type)
