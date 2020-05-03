634ad3d99e543f9cd5d927a7b1babebef48531af
Bram Jetten <mail@bramjetten.nl>
2020-05-03 18:51:54 +0000

    Spina image tag

--- app/helpers/spina/pages_helper.rb
+++ app/helpers/spina/pages_helper.rb
@@ -5,6 +5,13 @@ module Spina
       find_part(part_name, global)&.content
     end
 
+    def spina_image_tag(image, variant_options = {}, options = {})
+      image = find_part(image, global: options[:global]) unless image.is_a? Spina::Parts::Image
+      return "" unless image
+      image_tag_options = {alt: image.alt}
+      image_tag spina_image_url(image, variant_options), image_tag_options.merge(options)
+    end
+
     def spina_attachment_url(attachment, global: false)
       attachment = find_part(attachment, global) unless attachment.is_a?(Spina::Parts::Attachment)
       return "" unless attachment
@@ -12,7 +19,7 @@ module Spina
     end
 
     def spina_image_url(image, options = {})
-      image = find_part(image, options[:global]) unless image.is_a?(Spina::Parts::Image)
+      image = find_part(image, global: options[:global]) unless image.is_a? Spina::Parts::Image
       return "" unless image
       variant_key = ActiveStorage::Variation.encode(options)
       main_app.rails_blob_representation_url(image.signed_blob_id, variant_key, image.filename)
