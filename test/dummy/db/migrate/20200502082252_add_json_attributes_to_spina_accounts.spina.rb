302931aeebcbbcadc5e6ab817af4075d41832ab7
Bram Jetten <mail@bramjetten.nl>
2020-05-03 13:04:40 +0200

    Remove duplicate migrations

--- test/dummy/db/migrate/20200502082252_add_json_attributes_to_spina_accounts.spina.rb
+++
@@ -1,6 +0,0 @@
-# This migration comes from spina (originally 13)
-class AddJsonAttributesToSpinaAccounts < ActiveRecord::Migration[6.1]
-  def change
-    add_column :spina_accounts, :json_attributes, :jsonb
-  end
-end
