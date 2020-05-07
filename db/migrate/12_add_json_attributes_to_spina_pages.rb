c5402ee7535dbb554f9e81e9303399483ef379f1
Bram Jetten <mail@bramjetten.nl>
2020-05-07 19:53:08 +0200

    Change migrations to version 5.2

--- db/migrate/12_add_json_attributes_to_spina_pages.rb
+++ db/migrate/12_add_json_attributes_to_spina_pages.rb
@@ -1,4 +1,4 @@
-class AddJsonAttributesToSpinaPages < ActiveRecord::Migration[6.0]
+class AddJsonAttributesToSpinaPages < ActiveRecord::Migration[5.2]
   def change
     add_column :spina_pages, :json_attributes, :jsonb
   end
--- db/migrate/13_add_json_attributes_to_spina_accounts.rb
+++ db/migrate/13_add_json_attributes_to_spina_accounts.rb
@@ -1,4 +1,4 @@
-class AddJsonAttributesToSpinaAccounts < ActiveRecord::Migration[6.1]
+class AddJsonAttributesToSpinaAccounts < ActiveRecord::Migration[5.2]
   def change
     add_column :spina_accounts, :json_attributes, :jsonb
   end
