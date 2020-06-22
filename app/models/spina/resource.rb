8f0a7cfef5f7c507f97c564835bc071552324798
Bram Jetten <mail@bramjetten.nl>
2020-06-22 20:57:23 +0200

    Remove commented lines

--- app/models/spina/resource.rb
+++ app/models/spina/resource.rb
@@ -2,10 +2,6 @@ module Spina
   class Resource < ApplicationRecord
     has_many :pages, dependent: :restrict_with_exception
 
-    # belongs_to :parent_page, class_name: "Spina::Page", optional: true
-
-    # after_save :scope_pages_to_parent_page
-
     def pages
       case order_by
       when "title"
@@ -14,14 +10,6 @@ module Spina
         super.order(created_at: :desc)
       end
     end
-
-    # private
-
-    #   def scope_pages_to_parent_page
-    #     pages.roots.each do |root_page|
-    #       root_page.update(parent: parent_page)
-    #     end
-    #   end
-
+    
   end
 end
