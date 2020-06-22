a6ed79b4a322eeb362ec725f6c142df25b6ca570
Bram Jetten <mail@bramjetten.nl>
2020-06-22 21:55:17 +0200

    Remove test db migration

--- test/dummy/db/migrate/20200622192131_add_url_title_to_spina_page_translations.spina.rb
+++
@@ -1,6 +0,0 @@
-# This migration comes from spina (originally 20200622192008)
-class AddUrlTitleToSpinaPageTranslations < ActiveRecord::Migration[6.0]
-  def change
-    add_column :spina_page_translations, :url_title, :string
-  end
-end
