e58128a3d163288dc07aec234ccf1775b98b8727
Bram Jetten <mail@bramjetten.nl>
2020-07-27 09:49:26 +0200

    Fix Rails dependency to 6.0.3 (#604)

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_22_192131) do
+ActiveRecord::Schema.define(version: 2020_06_22_192132) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
@@ -34,14 +34,7 @@ ActiveRecord::Schema.define(version: 2020_06_22_192131) do
     t.bigint "byte_size", null: false
     t.string "checksum", null: false
     t.datetime "created_at", null: false
-    t.string "service_name", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
-  end
-
-  create_table "active_storage_variant_records", force: :cascade do |t|
-    t.bigint "blob_id", null: false
-    t.string "variation_digest", null: false
-    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
   create_table "spina_accounts", id: :serial, force: :cascade do |t|
@@ -274,7 +267,4 @@ ActiveRecord::Schema.define(version: 2020_06_22_192131) do
     t.string "password_reset_token"
     t.datetime "password_reset_sent_at"
   end
-
-  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
 end
