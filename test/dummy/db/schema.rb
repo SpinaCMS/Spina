b6356429d7acc672b57f76fe3ea174794628a418
Bram Jetten <mail@bramjetten.nl>
2021-01-08 20:29:51 +0100

    Update tests for Rails 6.1

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_22_192132) do
+ActiveRecord::Schema.define(version: 2021_01_08_191444) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
@@ -34,7 +34,14 @@ ActiveRecord::Schema.define(version: 2020_06_22_192132) do
     t.bigint "byte_size", null: false
     t.string "checksum", null: false
     t.datetime "created_at", null: false
+    t.string "service_name", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "active_storage_variant_records", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
   create_table "spina_accounts", id: :serial, force: :cascade do |t|
@@ -267,4 +274,6 @@ ActiveRecord::Schema.define(version: 2020_06_22_192132) do
     t.string "password_reset_token"
     t.datetime "password_reset_sent_at"
   end
+
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
 end
