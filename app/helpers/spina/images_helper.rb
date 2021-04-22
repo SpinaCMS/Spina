55d326048e901163fb1e8979ec5ea63fad9b6352
Jon Moniaci <jonmoniaci@gmail.com>
2021-04-22 14:49:05 -0500

    ActiveStorage variant definitions use ImageProcessing gem macros (#682)
    ActiveStorage variant definitions use ImageProcessing gem macros
    Co-authored-by: Bram Jetten <mail@bramjetten.nl>

--- app/helpers/spina/images_helper.rb
+++ app/helpers/spina/images_helper.rb
@@ -3,12 +3,19 @@ module Spina
 
     def thumbnail_url(image)
       return "" if image.nil?
-      main_app.url_for(image.variant(resize: "400x300^", crop: "400x300+0+0"))
+      main_app.url_for(image.variant(resize_to_fill: [400, 300]))
     end
 
     def embedded_image_url(image)
       return "" if image.nil?
-      main_app.url_for(image.variant(resize: Spina.config.embedded_image_size))
+
+      # support both ImageProcessing gem macro :resize_to_limit and ImageMagick-specific :resize
+      resize_key = if !Spina.config.embedded_image_size.is_a?(Array)
+                     :resize
+                   else
+                     :resize_to_limit
+                   end
+      main_app.url_for(image.variant(Hash[resize_key, Spina.config.embedded_image_size]))
     end
 
   end
--- lib/spina.rb
+++ lib/spina.rb
@@ -16,7 +16,7 @@ module Spina
                   :frontend_parent_controller,
                   :disable_frontend_routes,
                   :max_page_depth, 
-                  :locales, 
+                  :locales,
                   :embedded_image_size
 
   # Specify a backend path. Defaults to /admin.
@@ -35,6 +35,24 @@ module Spina
   # Images that are embedded in the Trix editor are resized to fit
   # You can optimize this for your website and go for a smaller (or larger) size
   # Default: 2000x2000px
-  self.embedded_image_size = "2000x2000>"
+  class << self
+    alias_method :config_original, :config
+    
+    def config
+      config_obj = self.config_original
+      
+      def config_obj.embedded_image_size=(image_size)
+        if image_size.is_a? String
+          ActiveSupport::Deprecation.warn("Spina embedded_image_size should be set to an array of arguments to be passed to the :resize_to_limit ImageProcessing macro. https://github.com/janko/image_processing/blob/master/doc/minimagick.md#resize_to_limit")
+        end
+        
+        self[:embedded_image_size] = image_size
+      end
+      
+      config_obj
+    end
+  end
+  
+  self.embedded_image_size = [2000, 2000]
 
 end
