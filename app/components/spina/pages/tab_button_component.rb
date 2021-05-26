68012f30e755c74a4ff2c73a52f0f4f16bab6998
Kenny Buckler <kbuckler@gmail.com>
2021-05-26 12:23:33 -0700

    Refactor tab buttons to view component

---
+++ app/components/spina/pages/tab_button_component.rb
@@ -0,0 +1,22 @@
+module Spina
+  module Pages
+    class TabButtonComponent < ApplicationComponent
+      def initialize(page:, locale:, tab_name:)
+        @page = page
+        @locale = locale
+        @tab_name = tab_name
+      end
+
+      def icon_name
+        case @tab_name
+        when 'page_content'
+          'document-text'
+        when 'search_engines'
+          'search'
+        when 'advanced'
+          'cog'
+        end
+      end
+    end
+  end
+end
