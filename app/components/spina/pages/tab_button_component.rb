767753b85954ee2e76892fc45581b7dcb5594f45
Bram Jetten <mail@bramjetten.nl>
2021-05-28 10:04:28 +0200

    Removed label slot from TabButtonComponent

--- app/components/spina/pages/tab_button_component.rb
+++ app/components/spina/pages/tab_button_component.rb
@@ -1,17 +1,11 @@
 module Spina
   module Pages
     class TabButtonComponent < ApplicationComponent
-      renders_one :label, "LabelComponent"
 
       def initialize(tab_name:)
         @tab_name = tab_name
       end
 
-      class LabelComponent < ApplicationComponent
-        def call
-          content
-        end
-      end
     end
   end
 end
