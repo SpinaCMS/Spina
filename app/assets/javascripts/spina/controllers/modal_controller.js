011fed9a71239f8e9ea27df186972fdf9fa9dc38
Bram Jetten <mail@bramjetten.nl>
2021-06-24 23:28:52 +0200

    Clear turbo frame src - fixes #726 (#727)
    * Clear turbo frame src - fixes #726

--- app/assets/javascripts/spina/controllers/modal_controller.js
+++ app/assets/javascripts/spina/controllers/modal_controller.js
@@ -8,10 +8,15 @@ export default class extends Controller {
 
   close() {
     this.element.remove()
+    this.modalTurboFrame.src = null
   }
   
   escClose(event) {
     if (event.key === 'Escape') this.close()
+  }
+  
+  get modalTurboFrame() {
+    return document.querySelector("turbo-frame[id='modal']")
   }
 
 }
