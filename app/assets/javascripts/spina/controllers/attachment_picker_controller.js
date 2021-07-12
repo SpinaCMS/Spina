f7bab39c7aafc6dbeb29fdfe8962e84590c4b2ae
Bram Jetten <mail@bramjetten.nl>
2021-07-12 19:52:57 +0200

    Stimulus controllers for attachment forms - Fixes #747 and #746 (#749)
    * Fix stuck upload button for attachments #747
    
    * Add stimulus controller to fix attachment picker #746
    
    * Update locales from LocaleApp
    
    * Update spanish locale
    
    * Update spanish locale

---
+++ app/assets/javascripts/spina/controllers/attachment_picker_controller.js
@@ -0,0 +1,15 @@
+import { Controller } from "stimulus"
+
+export default class extends Controller {
+  static get targets() {
+    return [ "signedBlobId", "filename" ]
+  }
+
+  pick(event) {
+    let select = event.currentTarget
+    let option = select.options[select.selectedIndex]
+    this.signedBlobIdTarget.value = option.dataset.signedBlobId || ""
+    this.filenameTarget.value = option.dataset.filename || ""
+  }
+  
+}
