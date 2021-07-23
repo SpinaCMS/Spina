d4d5c8d45bef304cb72c32df5c8cbdb85eb0ea38
Bram Jetten <mail@bramjetten.nl>
2021-07-23 15:06:34 +0200

    Session authentication test

---
+++ test/integration/spina/authentication/session_authentication_test.rb
@@ -0,0 +1,27 @@
+require 'test_helper'
+
+module Spina
+  module Authentication
+    class SessionAuthenticationTest < ActionDispatch::IntegrationTest
+      
+      setup do
+        host! "dummy.test"
+
+        @routes = Engine.routes
+        @account = FactoryBot.create :account
+        @user = FactoryBot.create :user
+      end
+      
+      test "visiting admin without logging in" do
+        get spina.admin_root_url
+        assert_redirected_to spina.admin_login_path
+      end
+
+      test "login" do
+        post "/admin/sessions", params: {email: @user.email, password: "password"}
+        assert_redirected_to spina.admin_root_url
+      end
+
+    end
+  end
+end
