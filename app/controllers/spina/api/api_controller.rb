1b598affc41a012b7fcc0202d32fcfb20f138608
Bram Jetten <mail@bramjetten.nl>
2021-08-01 11:35:34 +0200

    API

--- app/controllers/spina/api/api_controller.rb
+++ app/controllers/spina/api/api_controller.rb
@@ -1,22 +1,16 @@
 module Spina
   module Api
     class ApiController < ActionController::Base
+      include Spina::CurrentAccount, Spina::CurrentTheme
+      
       protect_from_forgery unless: -> { request.format.json? }
       
       rescue_from ActiveRecord::RecordNotFound, with: :render_404
       
-      TOKEN = Spina::Pro.config.api_key
+      TOKEN = Spina.config.api_key
       
       before_action -> { current_account && current_theme }
       before_action :authenticate_api
-      
-      def current_account
-        Spina::Current.account ||= Account.first
-      end
-      
-      def current_theme
-        Spina::Current.theme ||= Theme.find_by_name(current_account.theme)
-      end
       
       private
       
