02b025e79aa6ef3ea042c416e49aa029d29f53e5
Bram Jetten <mail@bramjetten.nl>
2021-08-01 16:28:41 +0200

    API tests

---
+++ test/integration/spina/api/images_test.rb
@@ -0,0 +1,23 @@
+require 'test_helper'
+
+module Spina
+  module Api
+    class ImagesTest < ActionDispatch::IntegrationTest
+      setup do
+        host! "dummy.test"
+
+        @routes = Engine.routes
+        @account = FactoryBot.create :account
+        
+        spina_png = fixture_file_upload("spina.png", 'image/png')
+        @image = Spina::Image.create(file: spina_png)
+      end
+      
+      test "show image" do
+        get "/api/images/#{@image.id}.json", headers: {'Authorization' => 'Token dummy_api_key'}
+        assert_not_empty @response.parsed_body.dig("data", "attributes", "url")
+      end
+
+    end
+  end
+end
--- test/integration/spina/api/pages_test.rb
+++ test/integration/spina/api/pages_test.rb
@@ -13,11 +13,24 @@ module Spina
       test "get all pages" do
         get "/api/pages.json", headers: {'Authorization' => 'Token dummy_api_key'}
         assert_not_empty @response.parsed_body.dig("data")
+        assert_not_empty @response.parsed_body.dig("meta")
       end
       
       test "homepage exists in api/pages" do
         get "/api/pages.json", headers: {'Authorization' => 'Token dummy_api_key'}
         assert_includes @response.parsed_body.dig("data").map{|data| data.dig("attributes", "name")}, "homepage"
+      end
+      
+      test "get page details" do
+        page = Spina::Page.first
+        get "/api/pages/#{page.id}.json", headers: {'Authorization' => 'Token dummy_api_key'}
+        assert_not_empty @response.parsed_body.dig("data", "attributes", "content")
+      end
+      
+      test "get page that does not exist" do
+        get "/api/pages/#{Spina::Page.maximum(:id) + 1}.json", headers: {'Authorization' => 'Token dummy_api_key'}
+        assert_response :not_found
+        assert_equal "Not Found", @response.parsed_body.dig("error")
       end
 
     end
