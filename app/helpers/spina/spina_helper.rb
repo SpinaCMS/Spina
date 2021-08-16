c3d51c921107c7c157a57b19ccf61413da3794c1
Bram Jetten <mail@bramjetten.nl>
2021-08-16 09:20:54 +0200

    Update to new Importmap API (#783)

--- app/helpers/spina/spina_helper.rb
+++ app/helpers/spina/spina_helper.rb
@@ -2,7 +2,7 @@ module Spina::SpinaHelper
 
   def spina_importmap_tags
     safe_join [
-      javascript_inline_importmap_tag(Spina.config.importmap.paths),
+      javascript_inline_importmap_tag(Spina.config.importmap),
       javascript_importmap_shim_tag,
       javascript_include_tag("spina/libraries/trix"),
       javascript_import_module_tag("application")
