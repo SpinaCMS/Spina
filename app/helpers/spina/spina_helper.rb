f70cfc13bd77daa6692a9f07137f0e76497c5eca
Bram Jetten <mail@bramjetten.nl>
2021-08-17 15:48:18 +0200

    Update importmap-rails and importmap (#788)
    * Update importmap-rails and importmap
    
    * Update stimulus-rails and turbo-rails

--- app/helpers/spina/spina_helper.rb
+++ app/helpers/spina/spina_helper.rb
@@ -1,11 +1,12 @@
 module Spina::SpinaHelper
 
-  def spina_importmap_tags
+  def spina_importmap_tags(entry_point = "application")
     safe_join [
-      javascript_inline_importmap_tag(Spina.config.importmap),
+      javascript_inline_importmap_tag(Spina.config.importmap.to_json(resolver: self)),
+      javascript_importmap_module_preload_tags(Spina.config.importmap),
       javascript_importmap_shim_tag,
       javascript_include_tag("spina/libraries/trix"),
-      javascript_import_module_tag("application")
+      javascript_import_module_tag(entry_point)
     ], "\n"
   end
   
