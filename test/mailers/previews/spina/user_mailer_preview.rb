417bcb53267d99f1b3671cd5f7c4297c392451ae
Bram Jetten <mail@bramjetten.nl>
2021-08-18 17:12:18 +0200

    Add multipart email forgot password

--- test/mailers/previews/spina/user_mailer_preview.rb
+++ test/mailers/previews/spina/user_mailer_preview.rb
@@ -2,7 +2,7 @@ module Spina
   class UserMailerPreview < ActionMailer::Preview
     
     def forgot_password
-      UserMailer.forgot_password(Spina::User.first, "macOS", "Safari")
+      UserMailer.forgot_password(Spina::User.first, "macOS", "Safari", format: :txt)
     end
     
   end
