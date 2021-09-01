3eab5155512d725a55866d312454204196efbbdb
Bram Jetten <mail@bramjetten.nl>
2021-09-01 09:50:11 +0200

    Move importmap to importmap.rb

--- lib/spina/engine.rb
+++ lib/spina/engine.rb
@@ -38,22 +38,6 @@ module Spina
         Spina::Parts::Option,
         Spina::Parts::Attachment
       )
-    end
-    
-    initializer "spina.importmap" do
-      Spina.config.importmap.draw do
-        # Stimulus & Turbo
-        pin "@hotwired/stimulus", to: "stimulus.js"
-        pin "@hotwired/stimulus-autoloader", to: "stimulus-autoloader.js"
-        pin "@hotwired/turbo-rails", to: "turbo.js"
-        
-        # Spina entrypoint
-        pin "application", to: "spina/application.js"
-        
-        pin_all_from Spina::Engine.root.join("app/assets/javascripts/spina/controllers"), under: "controllers", to: "spina/controllers"
-        pin_all_from Spina::Engine.root.join("app/assets/javascripts/spina/libraries"), under: "libraries", to: "spina/libraries"
-      end
-    end
-    
+    end    
   end
 end
