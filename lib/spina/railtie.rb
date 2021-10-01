88c54c604646cd908f4f1d73ba6fd4d371a17c77
Bram Jetten <mail@bramjetten.nl>
2021-10-01 14:00:20 +0200

    Add support for auto-reloading themes

--- lib/spina/railtie.rb
+++ lib/spina/railtie.rb
@@ -5,6 +5,16 @@ module Spina
       app.config.assets.precompile += %w(spina/manifest)
     end
     
+    initializer "spina.reloader" do |app|
+      reloader = Reloader.new
+      reloader.execute
+      
+      app.reloaders << reloader
+      app.reloader.to_run do
+        reloader.execute
+      end
+    end
+    
     ActiveSupport.on_load(:action_controller) do
       ::ActionController::Base.send(:include, Spina::AdminSectionable)
     end
---
+++ lib/spina/reloader.rb
@@ -0,0 +1,20 @@
+class Spina::Reloader
+  delegate :execute_if_updated, :execute, :updated?, to: :updater
+  
+  def reload!
+    theme_paths.each { |path| load path }
+  end
+  
+  private
+  
+    def updater
+      @updater ||= Rails.application.config.file_watcher.new(theme_paths) do 
+        reload!
+      end
+    end
+  
+    def theme_paths
+      Rails.root.glob("config/initializers/themes/*.rb") 
+    end
+  
+end
