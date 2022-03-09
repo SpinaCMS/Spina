08f4457f177530a65f50d5c9fc6bca9f2da7cac1
Moxvallix <moxvallix@gmail.com>
2022-03-09 10:53:23 +1030

    chore: add back current_account helper method, add deprecation warning

--- app/controllers/concerns/spina/current_spina_account.rb
+++ app/controllers/concerns/spina/current_spina_account.rb
@@ -5,10 +5,19 @@ module Spina
     included do
       before_action :current_spina_account
       helper_method :current_spina_account
+      helper_method :current_account
     end
   
     private
-    
+
+      def current_account
+        ActiveSupport::Deprecation.warn(
+          "#current_account is deprecated, due to a common authentication namespace conflict." \
+          "Please use #current_spina_account instead."
+        )
+        current_spina_account
+      end
+
       def current_spina_account
         Spina::Current.account ||= ::Spina::Account.first
       end
