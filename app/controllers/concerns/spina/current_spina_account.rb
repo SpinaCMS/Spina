cd1ffa866f14b4dbde709147b481790002b3e49d
Moxvallix <moxvallix@gmail.com>
2022-04-12 15:41:43 +0930

    fix: implemented recommended changes

--- app/controllers/concerns/spina/current_spina_account.rb
+++ app/controllers/concerns/spina/current_spina_account.rb
@@ -1,7 +1,7 @@
 module Spina
   module CurrentSpinaAccount
     extend ActiveSupport::Concern
-    
+
     included do
       before_action :current_spina_account
       helper_method :current_spina_account
@@ -10,19 +10,18 @@ module Spina
   
     private
 
-      unless Spina.disable_deprecated_methods
-        def current_account
-          ActiveSupport::Deprecation.warn(
-            "#current_account is deprecated, due to a common authentication namespace conflict. \n" \
-            "Please use #current_spina_account instead."
-          )
-          current_spina_account
-        end
-      end
+    def current_account
+      ActiveSupport::Deprecation.warn(
+        "#current_account is deprecated, due to a common authentication namespace conflict. \n" \
+        "Please use #current_spina_account instead."
+      )
+      current_spina_account
+    end
 
-      def current_spina_account
-        Spina::Current.account ||= ::Spina::Account.first
-      end
-      
+    def current_spina_account
+      Spina::Current.account ||= ::Spina::Account.first
+    end
+
+    remove_method :current_account if Spina.config.disable_current_account
   end
 end
