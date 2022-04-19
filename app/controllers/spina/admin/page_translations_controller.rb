ea13c6baabaf98b5bc4328de1b98da6ab2cb68f7
Bram Jetten <mail@bramjetten.nl>
2022-04-19 11:37:19 +0200

    Refactor page translations controller

--- app/controllers/spina/admin/page_translations_controller.rb
+++ app/controllers/spina/admin/page_translations_controller.rb
@@ -4,10 +4,9 @@ module Spina
       
       def destroy
         @translation = Spina::Page::Translation.find(params[:id])
-        @page = @translation.translated_model
         @translation.destroy
         flash[:info] = t("spina.page_translations.deleted")
-        redirect_to spina.edit_admin_page_path(@page), status: :see_other
+        redirect_to spina.edit_admin_page_path(@translation.translated_model), status: :see_other
       end
       
     end
