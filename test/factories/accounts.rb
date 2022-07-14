3845d25e08a24bc3dbd3f4cd40cb1c5df99fac67
Bram Jetten <mail@bramjetten.nl>
2022-07-14 09:20:55 +0200

    Test suite deprecation warning

--- test/factories/accounts.rb
+++ test/factories/accounts.rb
@@ -2,6 +2,6 @@ FactoryBot.define do
   factory :account, class: "Spina::Account" do
     name { 'My Website' }
     email { 'bram@spinacms.com' }
-    preferences { { theme: 'demo' } }
+    preferences { { "theme" => 'demo' } }
   end
 end
