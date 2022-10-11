15e9cff48911ae4583335577461b09405c2b38ce
Bram Jetten <mail@bramjetten.nl>
2022-10-11 10:31:55 +0200

    Deprecate the use of app/decorators

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -7,16 +7,17 @@ require "spina"
 
 module Dummy
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
     config.hosts << "dummy.puma"
     config.hosts << "dummy.test"
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    
+    overrides = "#{Rails.root}/app/overrides"
+    Rails.autoloaders.main.ignore(overrides)
+    config.to_prepare do
+      Dir.glob("#{overrides}/**/*_override.rb").each do |override|
+        load override
+      end
+    end
   end
 end
-
