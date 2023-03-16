3c0361d8fb8bac0fe99a8892998bbf9279a4f2bf
Jonathan Jacob <jjjscf@gmail.com>
2023-03-16 23:44:22 +0100

    changed first new rewrite rule to remove unnecessary rules

--- app/models/spina/page.rb
+++ app/models/spina/page.rb
@@ -115,7 +115,7 @@ module Spina
     end
 
     def rewrite_rule
-      RewriteRule.where(new_path: old_path, old_path: materialized_path).delete_all
+      RewriteRule.where(old_path: materialized_path).delete_all
       RewriteRule.where(new_path: old_path).update(new_path: materialized_path)
       RewriteRule.where(old_path: old_path).first_or_create.update(new_path: materialized_path) if old_path != materialized_path
     end
