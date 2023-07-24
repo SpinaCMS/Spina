1e01b84fc40c5f16f9f15a517205cf25d7307f2a
VanillaHafer <craig.hafer@hashrocket.com>
2023-07-24 15:58:13 -0400

    Updated the navigation_item form
    * Now uses toggleComponenet
    * Can now use custom urls

--- app/controllers/spina/admin/navigation_items_controller.rb
+++ app/controllers/spina/admin/navigation_items_controller.rb
@@ -37,7 +37,15 @@ module Spina
       private
 
       def navigation_item_params
-        params.require(:navigation_item).permit(:page_id, :parent_id).merge(navigation_id: @navigation.id)
+        permitted_params = params.require(:navigation_item).permit(:page_id, :url, :url_label, :parent_id).merge(navigation_id: @navigation.id)
+        if params[:navigation_item][:navigation_option] == "true"
+          permitted_params[:page_id] = nil
+        else
+          permitted_params[:url] = nil
+          permitted_params[:url_label] = nil
+        end
+
+        permitted_params
       end
 
       def set_navigation
