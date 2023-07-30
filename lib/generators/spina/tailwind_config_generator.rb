b868760a1ce8d784b32d59f96cde2de1d7642e7b
Sébastien Dubois <sedubois@users.noreply.github.com>
2023-07-30 15:46:01 +0200

    Exclude generated tailwind config from VCS

--- lib/generators/spina/tailwind_config_generator.rb
+++ lib/generators/spina/tailwind_config_generator.rb
@@ -3,7 +3,9 @@ module Spina
     source_root File.expand_path("../templates", __FILE__)
 
     def create_tailwind_config_file
-      template "app/assets/config/spina/tailwind.config.js"
+      filename = "/app/assets/config/spina/tailwind.config.js"
+      insert_into_file ".gitignore", filename
+      template filename
     end
   end
 end
