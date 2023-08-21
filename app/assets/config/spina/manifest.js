5d119f5536d678e21d72806797d764c634067abf
Bram Jetten <mail@bramjetten.nl>
2023-08-21 20:34:53 +0200

    Update Tailwind build

---
+++ app/assets/config/spina/manifest.js
@@ -0,0 +1,11 @@
+//= link_tree ../../images/spina
+//= link_tree ../../fonts/spina
+
+//= link_directory ../../javascripts/spina/controllers
+//= link_directory ../../javascripts/spina/libraries
+
+//= link spina/animate.css
+//= link spina/fonts.css
+//= link spina/tailwind.css
+
+//= link spina/application.js
