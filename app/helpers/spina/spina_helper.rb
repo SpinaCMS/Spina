bb746f5494c3cc94ab68f338cbef617e65a9c2aa
aseroff <andy@seroff.co>
2024-01-03 15:22:25 -0600

    remove importmap shims

--- app/helpers/spina/spina_helper.rb
+++ app/helpers/spina/spina_helper.rb
@@ -1,10 +1,8 @@
 module Spina::SpinaHelper
-  def spina_importmap_tags(entry_point = "application", shim: true)
+  def spina_importmap_tags(entry_point = "application")
     safe_join [
       javascript_inline_importmap_tag(Spina.config.importmap.to_json(resolver: self)),
       javascript_importmap_module_preload_tags(Spina.config.importmap),
-      (javascript_importmap_shim_nonce_configuration_tag if shim),
-      (javascript_importmap_shim_tag if shim),
       javascript_import_module_tag(entry_point)
     ], "\n"
   end
