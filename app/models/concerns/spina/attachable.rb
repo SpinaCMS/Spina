f3f9927a23c40461db9b89a6d0961ae5c6189621
Matthias Schmidt <mschmidt@pixelflush.com>
2024-08-01 11:11:12 +0200

    Add a with_library_path scope to Attachable

--- app/models/concerns/spina/attachable.rb
+++ app/models/concerns/spina/attachable.rb
@@ -11,6 +11,25 @@ module Spina
           "%" + Image.sanitize_sql_like(query) + "%"
         )
       end
+
+      scope :with_library_path, ->(query) do
+        folder_name, file_name = query.split("/")
+        unless file_name
+          file_name = folder_name
+          folder_name = nil
+        end
+  
+        relation = joins(:file_blob).where(
+          "active_storage_blobs.filename ILIKE ?",
+          "%" + Image.sanitize_sql_like(file_name) + "%"
+        )
+  
+        if folder_name
+          relation.joins(:media_folder).where("spina_media_folders.name = ?", folder_name)
+        else
+          relation
+        end
+      end
     end
     
     def name
