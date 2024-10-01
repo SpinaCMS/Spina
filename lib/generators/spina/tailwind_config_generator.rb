48da798a14a2ccc615b429191dea93ded843f381
Matt Polito <matt.polito@gmail.com>
2024-09-30 22:43:36 -0400

    fix: Continuous additions of tailwind config to gitinore

--- lib/generators/spina/tailwind_config_generator.rb
+++ lib/generators/spina/tailwind_config_generator.rb
@@ -2,14 +2,21 @@ module Spina
   class TailwindConfigGenerator < Rails::Generators::Base
     source_root File.expand_path("../templates", __FILE__)
 
-    def create_tailwind_config_file
-      filename = "app/assets/config/spina/tailwind.config.js"
-      template filename
-      insert_into_file ".gitignore", <<~TEXT
+    CONFIG_FILE = "app/assets/config/spina/tailwind.config.js"
+    IGNORE_TEXT = <<~TEXT.chomp
+      # Ignore auto-generated Spina Tailwind CSS configuration
+      /#{CONFIG_FILE}
+    TEXT
 
-        # Ignore auto-generated Spina Tailwind CSS configuration
-        /#{filename}
-      TEXT
+    def create_tailwind_config_file
+      template CONFIG_FILE
+
+      unless File.read(".gitignore").include?(IGNORE_TEXT)
+        append_to_file ".gitignore", <<~TEXT
+          \n
+          #{IGNORE_TEXT}
+        TEXT
+      end
     end
   end
 end
