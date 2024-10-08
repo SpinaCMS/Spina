218c08e4131fab82e85c93d1225283b7496867e8
Bram Jetten <mail@bramjetten.nl>
2024-10-08 22:14:09 +0200

    Fixtures

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -20,11 +20,17 @@ end
 FactoryBot.find_definitions
 
 # Load fixtures from the engine
-if ActiveSupport::TestCase.respond_to?(:fixture_path=)
-  ActiveSupport::TestCase.fixture_path = File.expand_path("fixtures", __dir__)
-  ActionDispatch::IntegrationTest.fixture_path = ActiveSupport::TestCase.fixture_path
-  ActiveSupport::TestCase.file_fixture_path = ActiveSupport::TestCase.fixture_path + "/files"
-  ActiveSupport::TestCase.fixtures :all
+# if ActiveSupport::TestCase.respond_to?(:fixture_path=)
+#   ActiveSupport::TestCase.fixture_path = File.expand_path("fixtures", __dir__)
+#   ActionDispatch::IntegrationTest.fixture_path = ActiveSupport::TestCase.fixture_path
+#   ActiveSupport::TestCase.file_fixture_path = ActiveSupport::TestCase.fixture_path + "/files"
+#   ActiveSupport::TestCase.fixtures :all
+# end
+class ActiveSupport::TestCase
+  self.fixture_path = File.expand_path('fixtures', __dir__)
+  self.file_fixture_path = fixture_path + '/files'
+
+  fixtures :all
 end
 
 module AuthenticationHelper
