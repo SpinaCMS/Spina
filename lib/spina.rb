1a51c36be53e38e844f0cf7a6937a695983af398
aseroff <aseroff@umn.edu>
2025-10-28 22:19:45 -0500

    Replace ActiveSupport::Configurable in Spina module

--- lib/spina.rb
+++ lib/spina.rb
@@ -14,50 +14,30 @@ require "spina/embeds"
 require "spina/embeds/trix_conversion"
 
 module Spina
-  include ActiveSupport::Configurable
-
   PARTS = []
   PLUGINS = []
   THEMES = []
 
-  config_accessor :api_key,
-    :api_path,
-    :authentication,
-    :backend_title,
-    :backend_path,
-    :importmap,
-    :frontend_parent_controller,
-    :disable_frontend_routes,
-    :disable_decorator_load,
-    :disable_current_account,
-    :locales,
-    :embedded_image_size,
-    :mailer_defaults,
-    :thumbnail_image_size,
-    :resource_pages_limit_value,
-    :party_pooper,
-    :tailwind_content,
-    :tailwind_plugins,
-    :queues,
-    :transliterations
-
-  # Defaults
-  self.api_key = nil
-  self.api_path = "api"
-  self.authentication = "Spina::Authentication::Sessions"
-  self.backend_title = "Spina CMS"
-  self.backend_path = "admin"
-  self.disable_frontend_routes = false
-  self.disable_decorator_load = false
-  self.disable_current_account = false
-  self.embedded_image_size = [2000, 2000]
-  self.mailer_defaults = ActiveSupport::OrderedOptions.new
-  self.thumbnail_image_size = [400, 400]
-  self.frontend_parent_controller = "ApplicationController"
-  self.locales = [I18n.default_locale]
-  self.resource_pages_limit_value = 25
-  self.party_pooper = false
-  self.transliterations = %i[latin]
+  mattr_accessor :api_key, default: nil
+  mattr_accessor :api_path, default: "api"
+  mattr_accessor :authentication, default: "Spina::Authentication::Sessions"
+  mattr_accessor :backend_title, default: "Spina CMS"
+  mattr_accessor :backend_path, default: "admin"
+  mattr_accessor :importmap
+  mattr_accessor :frontend_parent_controller, default: "ApplicationController"
+  mattr_accessor :disable_frontend_routes, default: false
+  mattr_accessor :disable_decorator_load, default: false
+  mattr_accessor :disable_current_account, default: false
+  mattr_accessor :locales, default: [I18n.default_locale]
+  mattr_accessor :embedded_image_size, default: [2000, 2000]
+  mattr_accessor :mailer_defaults, default: ActiveSupport::OrderedOptions.new
+  mattr_accessor :thumbnail_image_size, default: [400, 400]
+  mattr_accessor :resource_pages_limit_value, default: 25
+  mattr_accessor :party_pooper, default: false
+  mattr_accessor :tailwind_content
+  mattr_accessor :tailwind_plugins
+  mattr_accessor :queues
+  mattr_accessor :transliterations, default: %i[latin]
 
   # Queues for background jobs
   # - config.queues.page_updates
@@ -84,38 +64,71 @@ module Spina
   # You can optimize this for your website and go for a smaller (or larger) size
   # Default: 2000x2000px
   class << self
-    alias_method :config_original, :config
-
     def config
-      config_obj = config_original
+      @config ||= begin
+        config = ActiveSupport::OrderedOptions.new
 
-      def config_obj.tailwind_purge_content
-        deprecator.warn("config.tailwind_purge_content has been renamed to config.tailwind_content")
-        tailwind_content
-      end
-
-      def config_obj.tailwind_purge_content=(paths)
-        deprecator.warn("config.tailwind_purge_content has been renamed to config.tailwind_content")
-        self.tailwind_content = paths
-      end
-
-      def config_obj.embedded_image_size=(image_size)
-        if image_size.is_a? String
-          deprecator.warn("Spina embedded_image_size should be set to an array of arguments to be passed to the :resize_to_limit ImageProcessing macro. https://github.com/janko/image_processing/blob/master/doc/minimagick.md#resize_to_limit")
+        # Define method_missing to sync with module attributes
+        def config.method_missing(method, *args)
+          method_name = method.to_s
+          if method_name.end_with?('=')
+            # Setter
+            attr_name = method_name.chomp('=')
+            Spina.send("#{attr_name}=", args.first)
+            super
+          else
+            # Getter
+            return Spina.send(method) if Spina.respond_to?(method)
+            super
+          end
         end
 
-        self[:embedded_image_size] = image_size
-      end
+        def config.respond_to_missing?(method, include_private = false)
+          method_name = method.to_s
+          return true if method_name.end_with?('=') && Spina.respond_to?("#{method_name.chomp('=')}=")
+          Spina.respond_to?(method) || super
+        end
 
-      config_obj
+        # Special handling for deprecated methods
+        def config.tailwind_purge_content
+          Spina.deprecator.warn("config.tailwind_purge_content has been renamed to config.tailwind_content")
+          Spina.tailwind_content
+        end
+
+        def config.tailwind_purge_content=(paths)
+          Spina.deprecator.warn("config.tailwind_purge_content has been renamed to config.tailwind_content")
+          Spina.tailwind_content = paths
+        end
+
+        def config.embedded_image_size=(image_size)
+          if image_size.is_a? String
+            Spina.deprecator.warn("Spina embedded_image_size should be set to an array of arguments to be passed to the :resize_to_limit ImageProcessing macro. https://github.com/janko/image_processing/blob/master/doc/minimagick.md#resize_to_limit")
+          end
+          Spina.embedded_image_size = image_size
+        end
+
+        # Initialize with current values
+        Spina.instance_variables.each do |var|
+          if var.to_s.start_with?('@_mattr_')
+            key = var.to_s.sub('@_mattr_', '').to_sym
+            config[key] = Spina.instance_variable_get(var)
+          end
+        end
+
+        config
+      end
     end
 
     def deprecator
-      ActiveSupport::Deprecation.new("", "Spina")
+      @deprecator ||= ActiveSupport::Deprecation.new("", "Spina")
     end
 
     def mounted_at
       Spina::Engine.routes.find_script_name({})
+    end
+
+    def configure
+      yield config
     end
   end
 end
