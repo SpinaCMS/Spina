2df9b95130f0a0184eb27662884908123d890287
Alexis Szabo <alexis@appybara.com>
2026-02-08 10:57:29 -0800

    Make migration reversible
    remove_column requires specifying the column type to be reversible

--- db/migrate/5_remove_translated_columns.rb
+++ db/migrate/5_remove_translated_columns.rb
@@ -1,11 +1,11 @@
 class RemoveTranslatedColumns < ActiveRecord::Migration[5.0]
   def change
-    remove_column :spina_lines, :content
-    remove_column :spina_texts, :content
-    remove_column :spina_pages, :title
-    remove_column :spina_pages, :menu_title
-    remove_column :spina_pages, :description
-    remove_column :spina_pages, :seo_title
-    remove_column :spina_pages, :materialized_path
+    remove_column :spina_lines, :content, :string
+    remove_column :spina_texts, :content, :text
+    remove_column :spina_pages, :title, :string
+    remove_column :spina_pages, :menu_title, :string
+    remove_column :spina_pages, :description, :text
+    remove_column :spina_pages, :seo_title, :string
+    remove_column :spina_pages, :materialized_path, :string
   end
 end
